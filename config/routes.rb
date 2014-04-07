de24991741953e4585a299e321becceb05c7cc5f
Andu Morie <andumorie@gmail.com>
2014-04-08 02:48:44 +0300

    changed match to get on routes

--- config/routes.rb
+++ config/routes.rb
@@ -20,10 +20,10 @@ FmiCazare::Application.routes.draw do
   get '/logout', to: 'user_sessions#destroy'
 
   #pagini student
-  match '/creeaza_cerere', to: 'accommodations#new'
-  match '/profil_student', to: 'accommodations#student_profile'
-  match '/rezultate_verificare', to: 'accommodations#verified_results'
-  match '/decizia_comisiei', to: 'accommodations#decision'
-  match '/documente', to: 'accommodations#documents'
+  get '/creeaza_cerere', to: 'accommodations#new'
+  get '/profil_student', to: 'accommodations#student_profile'
+  get '/rezultate_verificare', to: 'accommodations#verified_results'
+  get '/decizia_comisiei', to: 'accommodations#decision'
+  get '/documente', to: 'accommodations#documents'
 
 end
